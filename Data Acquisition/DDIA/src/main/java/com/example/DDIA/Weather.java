315568b6708cc5a907d64c3fe8b8b1c1cfc423cf
ahme2001 <ahmedaboeleid30@gmail.com>
2023-05-18 13:41:07 +0300

    DWSM-8 handle bouns part of open meteo to get real data

---
+++ Data Acquisition/DDIA/src/main/java/com/example/DDIA/Weather.java
@@ -0,0 +1,32 @@
+package com.example.DDIA;
+
+import org.json.JSONArray;
+
+public class Weather {
+    private long timeStart;
+    private JSONArray temperature;
+    private JSONArray humidity;
+    private JSONArray windSpeed;
+
+    public Weather(long timeStart, JSONArray temperature, JSONArray humidity, JSONArray windSpeed) {
+        this.timeStart = timeStart;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+    }
+    public long getTimeStart() {
+        return timeStart;
+    }
+
+    public JSONArray getTemperature() {
+        return temperature;
+    }
+
+    public JSONArray getHumidity() {
+        return humidity;
+    }
+
+    public JSONArray getWindSpeed() {
+        return windSpeed;
+    }
+}
