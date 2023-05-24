1f0e67d138e0c5cc090da3e426d00d21f50aa4ee
ahme2001 <ahmedaboeleid30@gmail.com>
2023-05-24 20:19:02 +0300

    DWSM-3 implement Kafka Triggers Processors

---
+++ Data Acquisition/Kafka_Processor/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        RainingTriggerProcessor status = new RainingTriggerProcessor();
+        status.run();
+    }
+}
