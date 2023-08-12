28d47bc906730dc6fad0cc27e18e8c9831418bed
bazina <youssefbazina7@gmail.com>
2023-08-12 23:17:24 +0300

    DWSM-12 - Optimize lines

--- Base-Central-Station/src/main/java/baseCentralStation/BitCaskReader.java
+++ Base-Central-Station/src/main/java/baseCentralStation/BitCaskReader.java
@@ -3,6 +3,8 @@ package baseCentralStation;
 import baseCentralStation.Utilities.BitCaskClient;
 
 import java.io.IOException;
+import java.util.OptionalInt;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,18 +13,20 @@ public class BitCaskReader {
     public static void main(String[] args) throws IOException {
         BitCaskClient bitCaskClient = new BitCaskClient();
         bitCaskClient.startConnection("localhost", 4240);
+        bitCaskClient.open("/home/bazina/IdeaProjects/Weather-Stations-Monitoring/Base-Central-Station/src/main/resources/");
 
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
+        ScheduledExecutorService scheduledReader = Executors.newScheduledThreadPool(2);
+        scheduledReader.scheduleAtFixedRate(() -> {
             try {
                 for (int i = 1; i <= 10; i++) {
                     System.out.println("initiating get " + i);
                     String response = bitCaskClient.get(i);
                     System.out.println(response);
                 }
+                System.out.println();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }, 5, 5, TimeUnit.SECONDS);
+        }, 2, 5, TimeUnit.SECONDS);
     }
 }
