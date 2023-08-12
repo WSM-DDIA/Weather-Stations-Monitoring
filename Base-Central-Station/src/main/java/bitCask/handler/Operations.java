8069fbb4fb15ab791c73a354515f9fb863d23f25
bazina <youssefbazina7@gmail.com>
2023-08-12 22:50:11 +0300

    DWSM-12 - Add Operations constants for bitcask - Update directory names

--- Base-Central-Station/src/main/java/bitCask/handler/Operations.java
+++ Base-Central-Station/src/main/java/bitCask/handler/Operations.java
@@ -1,7 +1,8 @@
 package bitCask.handler;
 
-public class Constants {
+public class Operations {
     public static final byte OPEN = 0;
     public static final byte GET = 1;
     public static final byte SET = 2;
+    public static final byte DELETE = 3;
 }
